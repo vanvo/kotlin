d2d9400889d1a9ca6961fad52da994699b2ac1af
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-04-01 17:55:24 +0200

    FIR IDE: do not search for sealed class inheritors on every class resolve

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/SealedClassInheritors.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/SealedClassInheritors.kt
@@ -5,8 +5,32 @@
 
 package org.jetbrains.kotlin.fir.declarations
 
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.name.ClassId
 
-object SealedClassInheritorsKey : FirDeclarationDataKey()
 
-var FirRegularClass.sealedInheritors: List<ClassId>? by FirDeclarationDataRegistry.data(SealedClassInheritorsKey)
+abstract class SealedClassInheritorsProvider : FirSessionComponent {
+    abstract fun getSealedClassInheritors(firClass: FirRegularClass): List<ClassId>
+    abstract fun setSealedInheritors(firClass: FirRegularClass, inheritors: List<ClassId>)
+}
+
+class SealedClassInheritorsProviderImpl : SealedClassInheritorsProvider() {
+    private val inheritorsMap = hashMapOf<ClassId, List<ClassId>>()
+    override fun getSealedClassInheritors(firClass: FirRegularClass): List<ClassId> {
+        require(firClass.isSealed)
+        return inheritorsMap[firClass.classId] ?: emptyList()
+    }
+
+    override fun setSealedInheritors(firClass: FirRegularClass, inheritors: List<ClassId>) {
+        inheritorsMap[firClass.classId] = inheritors
+    }
+}
+
+private val FirSession.sealedClassInheritorsProvider: SealedClassInheritorsProvider by FirSession.sessionComponentAccessor()
+
+var FirRegularClass.sealedInheritors: List<ClassId>
+    get() = session.sealedClassInheritorsProvider.getSealedClassInheritors(this)
+    set(value) {
+        session.sealedClassInheritorsProvider.setSealedInheritors(this, value)
+    }
