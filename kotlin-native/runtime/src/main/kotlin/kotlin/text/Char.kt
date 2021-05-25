6b77777b5d95740f4fe9c4c37838a9387a81bebd
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2021-05-26 00:06:24 +0300

    Move ASCII check higher

--- kotlin-native/runtime/src/main/kotlin/kotlin/text/Char.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/text/Char.kt
@@ -256,9 +256,9 @@ private val digits = intArrayOf(
 
 internal actual fun digitOf(char: Char, radix: Int): Int = when {
     char >= '0' && char <= 'z' -> digits[char - '0']
+    char < '\u0080' -> -1
     char >= '\uFF21' && char <= '\uFF3A' -> char - '\uFF21' + 10 // full-width latin capital letter
     char >= '\uFF41' && char <= '\uFF5A' -> char - '\uFF41' + 10 // full-width latin small letter
-    char < '\u0080' -> -1
     else -> char.digitToIntImpl()
 }.let { if (it >= radix) -1 else it }
 
