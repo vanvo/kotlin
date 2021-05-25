a994e3a0b0980521ef95a5b2dafd1e95242bcc02
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-05-25 15:46:08 +0300

    [FIR] Create fir classes for java annotations and enums with Final modality

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProvider.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProvider.kt
@@ -236,8 +236,8 @@ class JavaSymbolProvider(
             name = javaClass.name
             val visibility = javaClass.visibility
             this@buildJavaClass.visibility = visibility
-            modality = javaClass.modality
             classKind = javaClass.classKind
+            modality = if (classKind == ClassKind.ANNOTATION_CLASS || classKind == ClassKind.ENUM_CLASS) Modality.FINAL else javaClass.modality
             this.isTopLevel = outerClassId == null
             isStatic = javaClass.isStatic
             this.javaTypeParameterStack = javaTypeParameterStack
@@ -263,7 +263,7 @@ class JavaSymbolProvider(
 
             status = FirResolvedDeclarationStatusImpl(
                 visibility,
-                javaClass.modality,
+                modality!!,
                 effectiveVisibility
             ).apply {
                 this.isInner = !isTopLevel && !this@buildJavaClass.isStatic
