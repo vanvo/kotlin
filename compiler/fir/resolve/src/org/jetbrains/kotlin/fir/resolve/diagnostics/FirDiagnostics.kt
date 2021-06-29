dc19e77a9fadc69d360398676ff14713c362b0b3
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2021-06-29 22:06:44 +0300

    [FIR] Implement FUNCTION_EXPECTED

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/diagnostics/FirDiagnostics.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/diagnostics/FirDiagnostics.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.fir.resolve.diagnostics
 import kotlinx.collections.immutable.ImmutableList
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
 import org.jetbrains.kotlin.fir.diagnostics.ConeDiagnostic
+import org.jetbrains.kotlin.fir.expressions.FirExpression
 import org.jetbrains.kotlin.fir.render
 import org.jetbrains.kotlin.fir.resolve.calls.Candidate
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
@@ -46,6 +47,10 @@ class ConeUnresolvedNameError(val name: Name) : ConeUnresolvedError() {
 
 class ConeFunctionCallExpectedError(val name: Name, val hasValueParameters: Boolean) : ConeDiagnostic() {
     override val reason: String get() = "Function call expected: $name(${if (hasValueParameters) "..." else ""})"
+}
+
+class ConeFunctionExpectedError(val expression: String, val type: ConeKotlinType) : ConeDiagnostic() {
+    override val reason: String get() = "Expression '$expression' of type '$type' cannot be invoked as a function"
 }
 
 class ConeHiddenCandidateError(
